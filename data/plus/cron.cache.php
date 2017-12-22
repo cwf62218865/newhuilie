1cfca9b97cdee77eb1479d7b9f057839ae8709ff
“zhangjing <2370401878@qq.com>
2017-12-22 09:05:21 +0800

    问问

--- data/plus/cron.cache.php
+++ data/plus/cron.cache.php
@@ -1,5 +1,5 @@
 <?php 
-$cron=array('0'=>array('id'=>'16','dir'=>'upjob.php','type'=>'3','week'=>'0','month'=>'0','hour'=>'12','minute'=>'0','nexttime'=>1513857600)
+$cron=array('0'=>array('id'=>'16','dir'=>'upjob.php','type'=>'3','week'=>'0','month'=>'0','hour'=>'12','minute'=>'0','nexttime'=>1513915200)
 )
 ; 
 $start=0; 
